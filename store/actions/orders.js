84f6336730d5e3ae927bda91d3f8786395791241
Edgar Bagdasarov <edbaga88@gmail.com>
2021-03-05 11:42:12 +0000

    test commit

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -7,7 +7,7 @@ export const fetchOrders = () => {
     return async dispatch => {
         try {
             const response = await fetch('https://online-shop-app-97090-default-rtdb.firebaseio.com/orders/u1.json');
-            console.log(response, ' response');
+            // console.log(response, ' response');
             if (!response.ok) {
                 throw new Error('Somethig went wrong!')
             }
@@ -19,7 +19,7 @@ export const fetchOrders = () => {
                     resData[key].totalAmount,
                     new Date(resData[key].date)))
             }
-            console.log(loadedOrders, ' orders');
+            // console.log(loadedOrders, ' orders');
             dispatch({ type: FETCH_ORDERS, fetchedOrders: loadedOrders })
 
         } catch (error) {
